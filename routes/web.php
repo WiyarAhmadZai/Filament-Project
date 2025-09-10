76708b7dbff2d891aa3648b78e28bc8142b0c861
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-11 00:59:14 +0430

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Redirect the login route to Filament's login page
+Route::get('/login', function () {
+    return redirect()->route('filament.wiyar.auth.login');
+})->name('login');
