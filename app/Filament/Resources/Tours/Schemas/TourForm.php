683db6e8fad06d9544a5275aa027a0c0648495fe
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-11 11:20:19 +0430

    created

---
+++ app/Filament/Resources/Tours/Schemas/TourForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Tours\Schemas;
+
+use Filament\Schemas\Schema;
+
+class TourForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
