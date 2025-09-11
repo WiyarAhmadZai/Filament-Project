79e64d4df8378e5e932995af59eff686af1ee83d
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-11 11:23:00 +0430

    created

---
+++ app/Livewire/NewsletterSubscription.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class NewsletterSubscription extends Component
+{
+    public function render()
+    {
+        return view('livewire.newsletter-subscription');
+    }
+}
