d66595cf9726fee91c0acb63ce5ba01ea88613ce
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-11 11:15:34 +0430

    updated

---
+++ database/migrations/2025_09_11_064052_create_tours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->integer('duration_days');
+            $table->string('difficulty_level'); // Easy, Moderate, Challenging
+            $table->json('highlights');
+            $table->json('itinerary');
+            $table->json('included_items');
+            $table->json('excluded_items');
+            $table->json('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tours');
+    }
+};
