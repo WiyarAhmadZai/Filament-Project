9c0f51f8f2863dde4b3897b7735f769e3199eae1
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-13 01:37:34 +0430

    updated

--- app/Filament/Admin/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Admin/Resources/Posts/Pages/CreatePost.php
@@ -4,8 +4,18 @@ namespace App\Filament\Admin\Resources\Posts\Pages;
 
 use App\Filament\Admin\Resources\Posts\PostResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (Auth::check() && !isset($data['user_id'])) {
+            $data['user_id'] = Auth::id();
+        }
+
+        return $data;
+    }
 }
