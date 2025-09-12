b64af2670df30aa484d86bf839e15cdf7d8e2733
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-13 01:04:26 +0430

    updated

--- app/Filament/Admin/Resources/Posts/PostResource.php
+++ app/Filament/Admin/Resources/Posts/PostResource.php
@@ -9,8 +9,16 @@ use App\Filament\Admin\Resources\Posts\Pages\ViewPost;
 use App\Filament\Admin\Resources\Posts\Schemas\PostForm;
 use App\Filament\Admin\Resources\Posts\Schemas\PostInfolist;
 use App\Filament\Admin\Resources\Posts\Tables\PostsTable;
+use App\Models\Category;
 use App\Models\Post;
 use BackedEnum;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
@@ -26,7 +34,18 @@ class PostResource extends Resource
 
     public static function form(Schema $schema): Schema
     {
-        return PostForm::configure($schema);
+        // return PostForm::configure($schema);
+        return $schema->schema([
+            TextInput::make('title')->required(),
+            TextInput::make('slug')->required(),
+            Select::make('category_id')
+            ->options(Category::all()->pluck('name', 'id')),
+            ColorPicker::make('color')->required(),
+            TagsInput::make('tags')->required(),
+            FileUpload::make('thumbnail')->disk('public')->directory('posts/thumbnails')->required(),
+            MarkdownEditor::make('content')->required(),
+            Checkbox::make('is_published')->required(),
+        ]);
     }
 
     public static function infolist(Schema $schema): Schema
