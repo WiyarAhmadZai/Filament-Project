615dc30f506950ac012818cd451b75e3399ee4d8
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-13 01:24:57 +0430

    updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
@@ -27,6 +28,15 @@ class Post extends Model
         'tags' => 'array',
         'is_published' => 'boolean',
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($post) {
+            if (Auth::check() && !$post->user_id) {
+                $post->user_id = Auth::id();
+            }
+        });
+    }
 
     public function user(): BelongsTo
     {
