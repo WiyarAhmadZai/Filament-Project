4c33cb369f0aeabf3f8f86adac3a5c8dc52c7822
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-13 01:13:13 +0430

    updated

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -2,7 +2,17 @@
 
 namespace App\Filament\Resources\Posts\Schemas;
 
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Set;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
 
 class PostForm
 {
@@ -10,7 +20,32 @@ class PostForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (Set $set, $state) {
+                        $set('slug', Str::slug($state));
+                    }),
+                TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required()
+                    ->searchable(),
+                ColorPicker::make('color')
+                    ->required(),
+                TagsInput::make('tags'),
+                FileUpload::make('thumbnail')
+                    ->image()
+                    ->directory('posts/thumbnails'),
+                RichEditor::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('is_published')
+                    ->required()
+                    ->default(true),
             ]);
     }
 }
