f79d616065e161bb323c1e149bc36842951298db
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-13 01:36:18 +0430

    ated

--- app/Filament/Resources/Posts/PostResource.php
+++ app/Filament/Resources/Posts/PostResource.php
@@ -9,10 +9,22 @@ use App\Filament\Resources\Posts\Schemas\PostForm;
 use App\Filament\Resources\Posts\Tables\PostsTable;
 use App\Models\Post;
 use BackedEnum;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
@@ -24,17 +36,53 @@ class PostResource extends Resource
 
     public static function form(Schema $schema): Schema
     {
-        return PostForm::configure($schema);
+        return $schema->schema([
+            TextInput::make('title')
+                ->required()
+                ->maxLength(255)
+                ->live(onBlur: true)
+                ->afterStateUpdated(function (Set $set, $state) {
+                    $set('slug', Str::slug($state));
+                }),
+            TextInput::make('slug')
+                ->required()
+                ->maxLength(255),
+            Select::make('category_id')
+                ->relationship('category', 'name')
+                ->required()
+                ->searchable(),
+            ColorPicker::make('color')
+                ->required(),
+            TagsInput::make('tags'),
+            FileUpload::make('thumbnail')
+                ->image()
+                ->directory('posts/thumbnails'),
+            RichEditor::make('content')
+                ->required()
+                ->columnSpanFull(),
+            Toggle::make('is_published')
+                ->required()
+                ->default(true),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->query(Post::with(['category', 'user']))
-            ->columns(PostsTable::configure($table)->getColumns())
-            ->filters(PostsTable::configure($table)->getFilters())
-            ->recordActions(PostsTable::configure($table)->getRecordActions())
-            ->toolbarActions(PostsTable::configure($table)->getToolbarActions());
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('category.name')
+                    ->label('Category')
+                    ->searchable()
+                    ->sortable(),
+                ImageColumn::make('thumbnail'),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ]);
     }
 
     public static function getRelations(): array
